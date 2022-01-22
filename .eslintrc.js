560a916e52d76d7fabae4b2d119cbad07b33fb56
Shigeo Hashimoto <sg0hsmt@gmail.com>
2022-01-22 16:42:12 +0900

    Add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     "plugin:vue/essential",
     "eslint:recommended",
@@ -10,12 +12,26 @@ module.exports = {
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint",
   ],
+
   parserOptions: {
     ecmaVersion: 2020,
   },
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "@typescript-eslint/no-loss-of-precision": "off", // require ESLint v7.1.0
   },
+
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
